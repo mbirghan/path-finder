47560c98bfdfae7bdd7835570638156b7d2611a4
Moritz Birghan <mb@mbirghan.com>
2025-01-28 15:06:18 +0100

    Add comment about expected behavior

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,20 @@
 import type { NextConfig } from "next";
 
+// /api and /app/api are both existing side by side
+// however /app/api takes priority over /api
+// the FastAPI entry point is /api/ so we need to make sure
+// /app/api/route.ts does not exist as it would override the FastAPI routes
+// the same goes for /app/api/py/fastapi/route.ts
+// this would override the FastAPI routes
+
+// hypothesis about routing behaviour:
+// NextJS takes the route from the request
+// if it exists in /app/api, it will use that
+// if not it will fallback to /api
+// FastAPI has one entry point /api/
+// so we need to route everything to /api/ that should go to FastAPI
+// this is done in the next.config.ts file with the rewrites
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
@@ -8,6 +23,8 @@ const nextConfig: NextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/:path*"
+            // We need to make sure that app/api/route.ts does not exist
+            // Otherwise, it will override the FastAPI routes
             : "/api/",
       },
       {
